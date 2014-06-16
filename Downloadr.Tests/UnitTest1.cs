e3d7f35e83aa0697400ebd9a95445ffaae36fe88
SondreB <pixelworld@hotmail.com>
2014-06-17 00:39:32 +0200

    Added unit test project for service
    Beginning of more tests for the service.

---
+++ Downloadr.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Downloadr.Services;
+
+namespace Downloadr.Tests
+{
+    [TestClass]
+    public class UserDataServiceTests
+    {
+        [TestMethod]
+        public void ShouldSaveUserData()
+        {
+            var service = new UserDataService();
+            var data = new Models.UserData() { ConnectionId = "1", Token = "token", TokenSecret = "secret" };
+            service.InsertOrUpdate(data);
+
+            //service.Retrieve();
+        }
+    }
+}
