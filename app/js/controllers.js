41b8a8ad4898611fe327d70816196cae9cdeb2e8
Sondre Bjellås <pixelworld@hotmail.com>
2014-07-06 09:46:37 +0200

    Minor change for test

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -112,9 +112,12 @@
         ];
 
 
+
         /* These will make properties available on the scope and auto-persist to local storage. */
         storage.bind($scope, 'language', 'en-US');
         storage.bind($scope, 'theme', 'dark');
+
+
 
     }]);
 
